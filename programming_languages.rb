1cd70a62c052363fed9352b97638728bd52460c1
Chovin Carlson <Chovin@users.noreply.github.com>
2018-09-12 06:20:11 +0000

    automatically backed up by learn

--- programming_languages.rb
+++ programming_languages.rb
@@ -1,9 +1,9 @@
 def reformat_languages(languages)
-  languages.reduce({}) do |hash, (style, langs)|  # => {:oo=>{:javascript=>{:type=>"interpreted"}}, :functional=>{:javascript=>{:type=>"interpreted"}}}
-    langs.each do |lang, attrs|                   # => {:javascript=>{:type=>"interpreted"}},                {:javascript=>{:type=>"interpreted"}}
-      hash[lang] ||= Hash[attrs]                  # => {:type=>"interpreted"},                               {:type=>"interpreted", :style=>[:oo]}
-      (hash[lang][:style] ||= []) << style        # => [:oo],                                                [:oo, :functional]
-    end                                           # => {:javascript=>{:type=>"interpreted"}},                {:javascript=>{:type=>"interpreted"}}
-    hash                                          # => {:javascript=>{:type=>"interpreted", :style=>[:oo]}}, {:javascript=>{:type=>"interpreted", :style=>[:oo, :functional]}}
-  end                                             # => {:javascript=>{:type=>"interpreted", :style=>[:oo, :functional]}}
-end                                               # => :reformat_languages
+  languages.reduce({}) do |hash, (style, langs)|
+    langs.each do |lang, attrs|
+      hash[lang] ||= Hash[attrs]
+      (hash[lang][:style] ||= []) << style
+    end
+    hash
+  end
+end
